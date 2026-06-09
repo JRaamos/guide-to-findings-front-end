9558ac2cc5d1e492e048c21a255470fc395c352a
Jonathan <jonathan@MacBook-Air-de-Jonathan.local>
2026-06-09 14:39:01 -0300

    add new value in env exemple

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,5 @@
 export const env = {
   apiUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337',
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://manualdosachados.com',
-  gaMeasurementId: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || '',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://manualdosachados.com.br',
+  gaMeasurementId: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || 'G-KYWQY6F4KC',
 };
